5ce793981bdc64274f9c20cfb3a023e9ea280291
benxdon <brandonlin2006@gmail.com>
2025-07-03 13:05:42 +0700

    web page returns the text

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from flask import Flask, request, render_template
 
+app = Flask(__name__)
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("form.html")
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    order_text = request.form.get("order_text")
+    print("Received order text:\n", order_text)
+    
+    return render_template("redir.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+'''
 scopes = [
     "https://www.googleapis.com/auth/spreadsheets"
 ]
@@ -12,3 +30,4 @@ workbook = client.open_by_key(sheet_id)
 
 sheets = workbook.worksheets()
 print(sheets)
+'''
